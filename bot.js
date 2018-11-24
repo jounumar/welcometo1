99ac88797a1efe5d63f66af404895dd4384f9666
jounumar <44790212+jounumar@users.noreply.github.com>
2018-11-25 01:43:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
       if(member.guild.id !== '474567474940084234') return;
     setTimeout(function() {
-    member.guild.channels.find(r => r.id === '507233340751872001').send('*Welcome To* ***Fèarless*** *Server*..');
+    member.guild.channels.find(r => r.id === '515891018088972290').send('*Welcome To* ***Fèarless*** *Server*..');
 },3000);
 });
 
